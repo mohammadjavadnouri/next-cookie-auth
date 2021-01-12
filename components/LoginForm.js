503e216dc753bb90943d2bd522de99f785f88a8d
Your Name <you@example.com>
2021-01-12 21:10:26 +0330

    create login form, handle post request on server

---
+++ components/LoginForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { loginUser } from "../lib/auth";
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  handleSubmit(evt) {
+    const { email, password } = this.state;
+    evt.preventDefault();
+    loginUser(email, password);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={this.handleChange}
+          ></input>
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={this.handleChange}
+          ></input>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
